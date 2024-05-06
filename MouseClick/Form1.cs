cc6d81285ca3d4fa7a6541bf579ea5ef39035348
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-05-06 14:34:35 +0300

    ok

--- MouseClick/Form1.cs
+++ MouseClick/Form1.cs
@@ -11,8 +11,24 @@ namespace MouseClick
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ClickMouse();
 
         }
+        private void ClickMouse()
+        {
+            int c = 0;
+            POINT p = new POINT();
+            while (true)
+            {
+                GetCursorPos (ref p);
+                ClientToScreen(Handle, ref p);
+                DoMouseLeftClick(p.x, p.y);
+                c++;
+                Thread.Sleep(200);
+                if (c == 50) break;
+            }
+        }
+
         [DllImport("user32.dll")]
         public static extern bool ClientToScreen(IntPtr hWnd, ref POINT pOINT);
         [StructLayout(LayoutKind.Sequential)]
@@ -22,7 +38,7 @@ namespace MouseClick
             public int y;
         }
         [DllImport ("user32.dll")]
-        public static extern void mose_event(int dsFlags, int dx, int dy,  int cButtons, int dcExtraInfo);
+        public static extern void mouse_event(int dsFlags, int dx, int dy,  int cButtons, int dcExtraInfo);
 
         public const int MOUSEEVENTF_LEFTDOWN = 0x02;
         public const int MOUSEEVENTF_LEFTUP = 0x04;
@@ -30,5 +46,27 @@ namespace MouseClick
         public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
         public const int MOUSEEVENTF_RIGHTUP = 0x10;
 
+        private void DoMouseLeftClick(int x, int y)
+        { 
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y,0,0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y,0,0);
+        }
+        private void DoMouseRightClick(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, x, y, 0, 0);
+        }
+        private void DoMouseDobleLeftClick(int x, int y)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, 0);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(ref POINT lpPoint);
+
     }
+
 }
